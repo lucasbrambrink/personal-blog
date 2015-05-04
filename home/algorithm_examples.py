5d0eccea3d7cd80d7b4ff84a701228fc5e846235
lucasbrambrink <lbrambrink@gmail.com>
2015-05-03 20:04:11 -0400

    added Euler Project algorithms

--- home/algorithm_examples.py
+++ home/algorithm_examples.py
@@ -2,6 +2,147 @@ __author__ = 'lb'
 
 import sys
 
+
+class Summation(object):
+
+    @staticmethod
+    def nth_digit_array(numbers=None, index=None):
+        return [int(x[index]) for x in numbers if len(x) > index]
+
+    @staticmethod
+    def sum(numbers=[]):
+        s = Summation
+        numbers = list(map(str, numbers))
+        final_sum = ''  # built as a string
+        remainder = 0
+        magitude = len(max(numbers, key=len))
+        for index in range(magitude):
+            array_sum = sum(s.nth_digit_array((n[::-1] for n in numbers), index))
+            current_sum = array_sum + remainder
+            if index + 1 < magitude:
+                final_sum += str(current_sum)[-1]
+                remainder = int(str(current_sum)[:-1]) if len(str(current_sum)) > 1 else 0
+            else:
+                final_sum += str(current_sum)[::-1]
+        return final_sum[::-1]
+
+
+
+class Multiplier(object):
+
+    @staticmethod
+    def multiply_as_string(num1, num2):
+        m = Multiplier
+        s = Summation
+
+        individual_sum = []
+        for index, digit in enumerate(reversed(str(num2))):
+            this_sum = m.multiply_through(int(digit), num1)
+            individual_sum.append(this_sum + '0' * index)
+
+        grand_sum = s.sum(individual_sum)
+
+        return grand_sum
+
+
+    @staticmethod
+    def multiply_through(digit, num):
+        digit, num = int(digit), str(num)
+        remainder = 0
+        product = ''
+        for index in range(len(num)):
+            current_sum = str(int(num[::-1][index]) * digit + int(str(remainder)[-1]))
+            if index < len(num) - 1:
+                product += current_sum[-1]
+                remainder = int(current_sum[:-1]) if len(str(current_sum)) > 1 else 0
+            else:
+                product += current_sum[::-1]
+        return product[::-1]
+
+
+
+
+
+
+class Euler(object):
+
+    def __init__(self, input_file=None, numbers=None):
+        self.file = input_file
+        self.numbers = self.load_numbers() if not numbers else numbers
+
+    def load_numbers(self):
+        if self.file:
+            with open(self.file, 'r') as numbers:
+                return [n for n in numbers.read().splitlines() if len(n)]
+
+
+    def nth_digit_array(self, n=None):
+        return [int(x[n]) for x in self.numbers]
+
+    @property
+    def grand_sum(self):
+        final_sum = '' # built as a string
+        remainder = 0
+        for digit in range(len(self.numbers[0]) - 1, -1, -1):
+            array_sum = sum(self.nth_digit_array(digit))
+            if digit > 0:
+                current_sum = array_sum + remainder
+                final_sum += str(current_sum)[-1]
+                remainder = int(str(current_sum)[:-1])
+            else:
+                current_sum = array_sum + int(str(remainder))
+                final_sum += str(current_sum)[::-1]
+        return final_sum[::-1]
+
+    @property
+    def first_ten(self):
+        return self.grand_sum[:10]
+
+    @staticmethod
+    def sum_digits_factorial(num):
+        if type(num) is not int:
+            num = int(num)
+        pairs = []
+        for x in range(1, int((num / 2) + 1)):
+            opposite = num + 1 - x
+            pairs.append(opposite * x)
+
+        print(pairs)
+        m = Multiplier
+        current_product = pairs[0]
+        for product in pairs[1:]:
+            current_product = m.multiply_as_string(current_product, str(product))
+            print(current_product)
+
+        return sum(map(int, current_product))
+
+        # return pairs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def algorithm_container():
 
     def nth_fibonacci(n, index=0, fib=0, next_fib=1):
@@ -126,7 +267,19 @@ def double_squares(number):
     return len(double_squares_found)
 
 if __name__ == "__main__":
-    with open(sys.argv[1], 'r') as test_cases:
-        for test in test_cases.read().splitlines()[1:]:
-            if len(test):
-                print double_squares(int(test))
+    # number1, number2 = 6112312231223423423423423424331231231231233, 43412312312312332342342342342342342341235
+    # number = number1 * number2
+    # print(number)
+    # e = Multiplier.multiply_as_string(number1, number2)
+    # print(e)
+    # assert e == str(number)
+    # e = Euler(sys.argv[1])
+    # print(e.grand_sum)
+    # print(len(e.grand_sum))
+    # print(e.first_ten)
+    sum_digits = Euler.sum_digits_factorial(int(sys.argv[1]))
+    print(sum_digits)
+    # prod = 1
+    # for x in range(10):
+    #     prod *= sum_digits[x]
+    #     print(prod)
